faf2a0205a5ea547142264c879ec5408c1f8aeda
Togem0n <413139475@qq.com>
2022-09-27 20:34:08 +0200

    ADD Hi Triangle exercise 2

--- LearnOpenGL/src/HelloWindow.cpp
+++ LearnOpenGL/src/HelloWindow.cpp
@@ -101,10 +101,13 @@ int main()
 	
 	// set up vertex data and buffers and configure vertex attributes
 	// --------------------------------------------------------------
-	float vertices[] = {
+	float firstTriangle[] = {
 		  0.1f, 0.1f, 0.0f,
 		 -0.1f, 0.1f, 0.0f,
 		  0.0f, 0.2f, 0.0f,
+	};
+
+	float secondTriangle[] = {
 		  0.1f, -0.1f, 0.0f,
 		  -0.1f, -0.1f, 0.0f,
 		  0.0f, -0.2f, 0.0f
@@ -114,39 +117,33 @@ int main()
 		0, 1, 3,
 		1, 2, 3
 	};
-	// Initialization code (done once unless object ned to be changed frequently)
-	// 1. bind Vertex Array Object (VAO)
-	// 2. copy our vertices array in a buffer for OpenGL to use (VBO operations)
-	// 3. then set our vertex attributes pointers (mapping)
-	unsigned int VBO, VAO, EBO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO); // generate a buffer to store the data we created before(vertices)
-	// bind the Vertex Array Object first, then bind and set vertex buffers, and then configure vertex attributes.
-	glGenBuffers(1, &EBO);
-	glBindVertexArray(VAO);
 
-	glBindBuffer(GL_ARRAY_BUFFER, VBO); // since buffer objects also has many different types, here we bind the data to the buffer.
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);// after we bind the buffer, we still need to send the data in.
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-	// now we stored the vertex data within memory on graphics card and managed by VBO
-	// which will be later processed by vertex shader and fragment shader.
+	// 1. generate vertex array and buffer (VAO and VBO)
+	// 2. bind vertex array
+	// 3. bind buffer and pass data in
+	// 4. map pointer vertex array and buffer
+	// 5. enable vertex array
 
-	// the reason why we need VAO is after we store the data in GPU, and specify the shader to process it
-	// OpenGL does not yet know how it should interpret the vertex data in memory and 
-	// how it should connect the vertex data to the vertex shader's attributes.
-	// thus we need to link the vertex attributes.
+	unsigned int VBOs[2], VAOs[2], EBO;
+	glGenVertexArrays(2, VAOs);
+	glGenBuffers(2, VBOs); 
+
+	// bind first triangle vao and vbo
+	glBindVertexArray(VAOs[0]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]); 
+	glBufferData(GL_ARRAY_BUFFER, sizeof(firstTriangle), firstTriangle, GL_STATIC_DRAW); 
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	// the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex object so afterwards we can safely unbind
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	// bind second triangle vao and vbo
+	glBindVertexArray(VAOs[1]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(secondTriangle), secondTriangle, GL_STATIC_DRAW);
 
-	// you can unbind VAO afterwards so other VAO calls wont accidentally modify this VAO but this rarely happens.
-	// modifying other VAOs requires a call to glBindVertexArray anyways so we generally dont unbind VAOs (nor VBOs) when it's not necessary.
-	glBindVertexArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
 
 	glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
 	// render loop
@@ -164,9 +161,13 @@ int main()
 
 		// draw first triangle ^^
 		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO);
-	    glDrawArrays(GL_TRIANGLES, 0, 6);
-		// glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+
+		glBindVertexArray(VAOs[0]);
+	    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		// draw second triangle ^^
+		glBindVertexArray(VAOs[1]);
+	    glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		// check and call events and swap the buffers
 		// ------------------------------------------
@@ -174,8 +175,8 @@ int main()
 		glfwPollEvents();
 	}
 
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
+	glDeleteVertexArrays(2, VAOs);
+	glDeleteBuffers(2, VBOs);
 	glDeleteProgram(shaderProgram);
 
 	glfwTerminate();
